763b71d81685d6d3f315c06a3426744d0808f3be
aidyncodes <aidynsomerville@gmail.com>
2026-02-18 15:53:41 -0500

    Adjusted code

--- src/main/scala/project1/WineQualityRegression.scala
+++ src/main/scala/project1/WineQualityRegression.scala
@@ -3,9 +3,10 @@ package project1
 import scalation.theory._
 
 object WineQualityRegression {
-  val filePath = "../../../data/project1/wine-quality-red.csv"
+  // scala already places "data" at beginning of path 
+  val filePath = "project1/wine-quality-red.csv"
   val dataName = "WineQualityRegression"
-  val data: Dataset = Dataset(dataName, filePath, 12, Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), 12)
+  val data: Dataset = Dataset(dataName, filePath, 12, Array(0,1,2,3,4,5,6,7,8,9,10), 11)
 
   @main def run(): Unit = {
     val utils = new Project1Utils(data.x, data.y, data.fname)
